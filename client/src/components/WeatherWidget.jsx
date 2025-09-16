739aed3c5faa6d0a50e3c69169299d092f765b63
Ambreen <ambreenambi04@gmail.com>
2025-09-16 17:37:12 +0500

    Fetching Weather Data Now!

--- client/src/components/WeatherWidget.jsx
+++ client/src/components/WeatherWidget.jsx
@@ -22,20 +22,23 @@ useEffect(() => {
 }, [API_KEY]);
 
 if (isLoading) {
-    return <motion.div className= "container card col-md-8 mt-3 py-3"><motion.h4 className= "text-danger fw-bold">Loading...</motion.h4></motion.div>
+    return <motion.div className= "container card col-md-8 mt-3 py-3 px-3"><motion.h4 className= "text-danger fw-bold" whileHover={{ scale: 1.1, color: "#66524bff" }}
+        transition={{ type: "spring", stiffness: 200 }}>Loading...</motion.h4></motion.div>
 }
 if (!weather || parseInt(weather.cod) !== 200) {
-    return <motion.div className= "container card col-md-8 mt-3 py-3 text-danger"><motion.h3>Failed to load weather data.</motion.h3></motion.div>
+    return <motion.div className= "container card col-md-8 mt-3 py-3 px-3 text-danger"><motion.h3 whileHover={{ scale: 1.1, color: "#862020ff" }}
+        transition={{ type: "spring", stiffness: 200 }}>Failed to load weather data.</motion.h3></motion.div>
 }
 
 return (
     <motion.div
-      className="weather-widget p-3 mb-3 cold-md-8"
+      className="card container col-md-8 mt-3 p-3 mb-3 cold-md-8"
       initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
+        whileHover={{ scale: 1.1, color: "#66524bff" }}
     >
-        <motion.h2>Weather Widget</motion.h2>
+        <motion.h2 className= "">Weather Widget</motion.h2>
         <motion.h3>{ weather.name }</motion.h3>
         <motion.h3>{ Math.round(weather.main.temp) }°C</motion.h3>
         <motion.h3>{ weather.weather[0].description }</motion.h3>
