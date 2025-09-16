23fb25f788afe0eb7eea18d7c94ef8ecd5bc67a6
Ambreen <ambreenambi04@gmail.com>
2025-09-16 12:01:43 +0500

    Initialized NodeJs Backend!

---
+++ client/components/ProfileCard.jsx
@@ -0,0 +1,11 @@
+import {motion} from 'framer-motion';
+
+const ProfileCard = ( { name, bio, avatar, links }) => {
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default ProfileCard;
